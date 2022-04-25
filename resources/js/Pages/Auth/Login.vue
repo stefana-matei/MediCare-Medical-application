cd7ba47e018683abc05b76fbd95adb96885dc8c1
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-04-25 16:26:27 +0300

    update frontend

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -55,7 +55,7 @@ const submit = () => {
 
             <div class="flex items-center justify-end mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
+                    Forgot your password????
                 </Link>
 
                 <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
