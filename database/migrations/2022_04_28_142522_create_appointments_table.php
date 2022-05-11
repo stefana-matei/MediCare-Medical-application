01608dfdc3f318fa3ad33afeb776763e06fb2b99
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-05-12 02:12:40 +0300

    Create 2022_04_28_142522_create_appointments_table.php

---
+++ database/migrations/2022_04_28_142522_create_appointments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('membership_id');
+            $table->timestamp('date');
+            //$table->string('location');
+            $table->string('specialization');
+            $table->boolean('honored')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
