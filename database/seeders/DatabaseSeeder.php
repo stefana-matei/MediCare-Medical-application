5286bd7b2d4093959ee66a3c5fa4d7018fa9a7fd
Stefana Matei <stefana.otilia.matei@gmail.com>
2023-01-17 21:57:38 +0200

    EDIT seeder files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PatientPOVSeeder::class,
-            MedicPOVSeeder::class
+            MedicPOVSeeder::class,
+            PatientPOVSeeder::class
         ]);
 
     }
--- database/seeders/PatientPOVSeeder.php
+++ database/seeders/PatientPOVSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Appointment;
 use App\Models\Level;
 use App\Models\Membership;
+use App\Models\Record;
+use App\Models\SettingPatient;
 use App\Models\Specialty;
 use App\Models\User;
 use App\Models\Visit;
@@ -26,63 +28,28 @@ class PatientPOVSeeder extends Seeder
         /** @var Generator $faker */
         $faker = Container::getInstance()->make(Generator::class);
 
+
         // Medics
         /** @var User $medic */
-        $medic = User::factory()->create([
-            'role' => 'medic',
-            'firstname' => 'Andrei',
-            'lastname' => 'David',
-            'email' => 'david.andrei@medicare.com',
-            'password' => Hash::make('secret123')
-        ]);
-
         /** @var User $medic2 */
-        $medic2 = User::factory()->create([
-            'role' => 'medic',
-            'firstname' => 'Andreea',
-            'lastname' => 'Alexandru',
-            'email' => 'andreea.alexandru@medicare.com',
-            'password' => Hash::make('secret123')
-        ]);
-
         /** @var User $medic3 */
-        $medic3 = User::factory()->create([
-            'role' => 'medic',
-            'firstname' => 'Alina',
-            'lastname' => 'Anghelus',
-            'email' => 'alina.anghelus@medicare.com',
-            'password' => Hash::make('secret123')
-        ]);
-
         /** @var User $medic4 */
-        $medic4 = User::factory()->create([
-            'role' => 'medic',
-            'firstname' => 'Adina Ioana',
-            'lastname' => 'Popescu',
-            'email' => 'adina.i.popescu@medicare.com',
-            'password' => Hash::make('secret123')
-        ]);
-
         /** @var User $medic5 */
-        $medic5 = User::factory()->create([
-            'role' => 'medic',
-            'firstname' => 'Ioan',
-            'lastname' => 'Baciu',
-            'email' => 'ioan.baciu@medicare.com',
-            'password' => Hash::make('secret123')
-        ]);
-
         /** @var User $medic6 */
-        $medic6 = User::factory()->create([
-            'role' => 'medic',
-            'firstname' => 'Marius',
-            'lastname' => 'Balea',
-            'email' => 'marius.balea@medicare.com',
-            'password' => Hash::make('secret123')
-        ]);
+
+        $medic = User::find(1);
+        $medic2 = User::find(2);
+        $medic3 = User::find(3);
+        $medic4 = User::find(4);
+        $medic5 = User::find(5);
+        $medic6 = User::find(6);
+
 
         // Patients
         /** @var User $patient */
+        /** @var User $patient2 */
+        /** @var User $patient3 */
+
         $patient = User::factory()->create([
             'role' => 'patient',
             'firstname' => 'Stefana',
@@ -91,33 +58,52 @@ class PatientPOVSeeder extends Seeder
             'password' => Hash::make('secret123')
         ]);
 
+        $patient2 = User::factory()->create([
+            'role' => 'patient',
+            'firstname' => 'Andromeda',
+            'lastname' => 'Gheorghescu',
+            'email' => 'andromeda.gheorghescu@medicare.com',
+            'password' => Hash::make('secret123')
+        ]);
+
+        $patient3 = User::factory()->create([
+            'role' => 'patient',
+            'firstname' => 'Beatrice',
+            'lastname' => 'Ionela',
+            'email' => 'beatrice.ionela@medicare.com',
+            'password' => Hash::make('secret123')
+        ]);
+
 //        User::factory(20)->create();
 
-
-        // Avatars
-        $medic->addMediaFromUrl('https://i.imgur.com/ViyDFni.jpg')->toMediaCollection('avatars');
-        $medic2->addMediaFromUrl('https://i.imgur.com/jWRAc7O.jpg')->toMediaCollection('avatars');
-        $medic3->addMediaFromUrl('https://i.imgur.com/PA96UXv.jpeg')->toMediaCollection('avatars');
-        $medic4->addMediaFromUrl('https://i.imgur.com/xWl3Ohm.jpeg')->toMediaCollection('avatars');
-        $medic5->addMediaFromUrl('https://i.imgur.com/9fAyvhQ.jpeg')->toMediaCollection('avatars');
-        $medic6->addMediaFromUrl('https://i.imgur.com/2OqRqYU.jpeg')->toMediaCollection('avatars');
-
-
         // Memberships
-        // $patient
         /** @var Membership $membership */
+        /** @var Membership $membership2 */
+        /** @var Membership $membership3 */
+        /** @var Membership $membership4 */
+        /** @var Membership $membership5 */
+
+        // for $patient
         $membership = $medic->memberships()->create([
             'patient_id' => $patient->id
         ]);
 
-        /** @var Membership $membership2 */
         $membership2 = $medic2->memberships()->create([
             'patient_id' => $patient->id
         ]);
 
-        /** @var Membership $membership3 */
         $membership3 = $medic3->memberships()->create([
             'patient_id' => $patient->id
+        ]);
+
+        // $patient2
+        $membership4 = $medic->memberships()->create([
+            'patient_id' => $patient2->id
+        ]);
+
+        // $patient3
+        $membership5 = $medic->memberships()->create([
+            'patient_id' => $patient3->id
         ]);
 
 
@@ -131,6 +117,7 @@ class PatientPOVSeeder extends Seeder
 
 
         // Programari in asteptare
+        // Important: pentru a fi in astepare => 'confirmed' = null
         $membership->appointments()->create([
             'date' => now()->addMonth(),
             'honored' => true
@@ -169,11 +156,24 @@ class PatientPOVSeeder extends Seeder
             'honored' => true
         ]);
 
+        $appointment6 = $membership4->appointments()->create([
+            'date' => now()->subMonth(),
+            'honored' => true,
+            'confirmed' => true
+        ]);
+
+
         // neonorata
         $appointment4 = $membership2->appointments()->create([
             'date' => now()->subMonth(),
             'confirmed' => true,
             'honored' => false
+        ]);
+
+        $appointment7 = $membership5->appointments()->create([
+            'date' => now()->addMonth(),
+            'honored' => false,
+            'confirmed' => true
         ]);
 
 
@@ -208,8 +208,17 @@ class PatientPOVSeeder extends Seeder
             'appointment_id' => $appointment3->id
         ]);
 
+        $visit4 = $membership4->visits()->create([
+            'date' => $appointment6->date,
+            'appointment_id' => $appointment6->id
+        ]);
+
 
         // Records
+        /** @var Record $record */
+        /** @var Record $record2 */
+        /** @var Record $record3 */
+
         $record = $visit->record()->create([
             'medical_history' => $faker->text(2000),
             'symptoms' => $faker->text(2000),
@@ -221,8 +230,7 @@ class PatientPOVSeeder extends Seeder
             'date_processed' => now()->subDay(7)
         ]);
 
-
-        $record3 = $visit3->record()->create([
+        $record2 = $visit3->record()->create([
             'medical_history' => $faker->text(),
             'symptoms' => $faker->text(),
             'diagnosis' => $faker->text(),
@@ -232,93 +240,20 @@ class PatientPOVSeeder extends Seeder
             'date_processed' => now()->subDay(7)
         ]);
 
+//        $record3 = $visit4->record()->create([
+//            'medical_history' => $faker->text(),
+//            'symptoms' => $faker->text(),
+//            'diagnosis' => $faker->text(),
+//            'para_clinical_data' => $faker->text(),
+//            'referral' => true,
+//            'indications' => $faker->text(),
+//            'date_processed' => now()->subDay(7)
+//        ]);
 
-        $allergology = Specialty::factory()->create(['name' => "Alergologie şi imunologie"]);
-        $laboratory_analysis = Specialty::factory()->create(['name' => "Analize laborator"]);
-        $cardiology = Specialty::factory()->create(['name' => "Cardiologie"]);
-        $dermatovenereology = Specialty::factory()->create(['name' => "Dermatovenerologie"]);
-        $diabetology = Specialty::factory()->create(['name' => "Diabet zaharat, nutriţie şi boli metabolice"]);
-        $endocrinology = Specialty::factory()->create(['name' => "Endocrinologie"]);
-        $gastroenterology = Specialty::factory()->create(['name' => "Gastroenterologie"]);
-        $haematology = Specialty::factory()->create(['name' => "Hematologie"]);
-        $kinetotherapy = Specialty::factory()->create(['name' => "Kinetoterapie"]);
-        $family_medicine = Specialty::factory()->create(['name' => "Medicină de familie"]);
-        $obstetrics_gynaecology = Specialty::factory()->create(['name' => "Obstetrică - ginecologie"]);
-        $paediatrics = Specialty::factory()->create(['name' => "Pediatrie"]);
-        $pulmonology = Specialty::factory()->create(['name' => "Pneumologie"]);
-        $psychiatry = Specialty::factory()->create(['name' => "Psihiatrie"]);
-        $psychology = Specialty::factory()->create(['name' => "Psihologie"]);
-        $radiology_imaging = Specialty::factory()->create(['name' => "Radiologie - imagistică medicală"]);
-        $radiotherapy = Specialty::factory()->create(['name' => "Radioterapie"]);
-        $rheumatology = Specialty::factory()->create(['name' => "Reumatologie"]);
-
-
-        $primary = Level::factory()->create(['name' => 'Medic primar']);
-        $specialist = Level::factory()->create(['name' => 'Medic specialist']);
-
-        // Settings
-        $settingMedic = $medic->settingsMedic()->create([
-            'level_id' => $primary->id,
-            'specialty_id' => $cardiology->id,
-            'skills' => 'Ecografie abdominala' . PHP_EOL . 'Ecocardiografie transtoracica',
-            'areas_of_activity' => 'Ecografie cardiaca',
-            'postgraduate_courses' => '2015: Al 54-lea Congres National de Cardiologie, Sinaia' .
-                PHP_EOL . '2011: Curs Ecocardiografie Transesofagiana in practica clinica, Timisoara' .
-                PHP_EOL . '2009: Curs Cum tratam hipertensiunea arteriala',
-            'member' => 'Societatea Romana de Cardiologie'
-        ]);
-
-        $settingMedic2 = $medic2->settingsMedic()->create([
-            'level_id' => $specialist->id,
-            'specialty_id' => $cardiology->id,
-            'skills' => 'Cardiologie pediatrica' . PHP_EOL . 'Ecografie',
-            'education' => 'Ianuarie 2016 - prezent: Medic Specialist Cardiolog in cadrul Clinicii de Cardiologie a Spitalului Clinic de Urgenta Bucuresti' .
-                PHP_EOL . 'Iunie 2012 – Decembrie 2015: Spitalul Clinic de Urgenta Bucuresti, Medic rezident, Sectia Cardiologie' .
-                PHP_EOL . '2004-2010: Facultatea de Medicina si Farmacie “Iuliu Hatieganu” Cluj-Napoca, Romania',
-            'postgraduate_courses' => '2014: EHRA Course: Cardiac Pacing, ICD and Cardiac Resynchronisation, Viena, Austria.' .
-                PHP_EOL . '2012: “Cardiologists of Tomorrow” grant oferit de Societate Europeana de Cardiologie(ESC) pentru participarea a ce de-al 60-lea congres ESC, Munchen 24-29 August 2012' .
-                PHP_EOL . '2011: Basis of Good Clinical Practice for Investigators, Verum.edu symposia, 11 Aprilie 2011, Bucuresti, Romania',
-            'trainings' => '2010 Ianuarie: stagiu de Obstetrica/Ginecologie, Univ.-Frauenklinik Heidelberg, Germania' .
-                PHP_EOL . '2007 August: Hospital de Santa Maria, sectia de Medicina Interna, Lisabona, Portugalia' .
-                PHP_EOL . '2005 Noiembrie: Centre l’Hopitalier Regional de Meulan-Les Mureaux, sectia de Pneumologie, Paris, Franta',
-            'member' => 'Societatea Europeana de Cardiologie' . PHP_EOL . 'Asociatia Europeana de Aritmologie' .
-                PHP_EOL . 'Asociatia Europeana de Imagistica Cardiovasculara' .
-                PHP_EOL . 'Societatea Europeana de Cardiologie'
-        ]);
-
-        $settingMedic3 = $medic3->settingsMedic()->create([
-            'level_id' => $primary->id,
-            'specialty_id' => $endocrinology->id
-        ]);
-
-        $settingMedic4 = $medic4->settingsMedic()->create([
-            'level_id' => $primary->id,
-            'specialty_id' => $gastroenterology->id
-        ]);
-
-        $settingMedic5 = $medic5->settingsMedic()->create([
-            'level_id' => $specialist->id,
-            'specialty_id' => $dermatovenereology->id
-        ]);
-
-        $settingMedic6 = $medic6->settingsMedic()->create([
-            'level_id' => $primary->id,
-            'specialty_id' => $obstetrics_gynaecology->id,
-            'specialisation' => 'Chirurgia endometriozei (peste 100 de cazuri de endometrioza avansata operate pe an, cu rezectii colorectale, cu rezectii de vezica si alte tipuri de interventii similare)',
-            'skills' => 'competenta in ecografie obstetrica-ginecologie' .
-                PHP_EOL . 'competenta in histeroscopie' .
-                PHP_EOL . 'competenta in laparoscopie',
-            'trainings' => 'Chirurgie avansata in domeniul endometriozei, Prof. Hudelist, Viena, Austria' .
-                PHP_EOL . 'Chirurgice avansata in domeniul endometriozei, Prof. Horace Roman, Rouen, France' .
-                PHP_EOL . 'Chirururgie avansata in domeniul endometriozei, Prof. Keckstein, Villach, Austria',
-            'international_certifications' => 'Certificare Fetal Medical Foundation, London (FMF)' .
-                PHP_EOL . 'EndoCert, Centrul de Excelenta Endo Institute acreditat de Liga Europeana de Endometrioza',
-            'publications' => 'Pelvic nerves Endometriosis, EEL 2018' .
-                PHP_EOL . 'Pregnancy after deep endometriosis surgery –National Congress of Obstetrics and Gynecology, 2018' .
-                PHP_EOL . 'First Romanan experience as a center for deep Endometriosis surgery, SEUD Pars, 2015',
-            'other_realizations' => 'Proiecte: ' . PHP_EOL . 'Cofondator Spitalul Premiere, cel mai mare spital privat din regiunea de Vest a Romaniei' .
-                PHP_EOL . 'Fondator al Centrului de Excelenta EndoInstitute Timisoara'
-        ]);
+        // SettingsPatient
+        /** @var SettingPatient $settingPatient */
+        /** @var SettingPatient $settingPatient2 */
+        /** @var SettingPatient $settingPatient3 */
 
         $settingPatient = $patient->settingsPatient()->create([
             'pin' => '2880822426702',
@@ -329,6 +264,28 @@ class PatientPOVSeeder extends Seeder
             'city' => 'Timisoara',
             'address' => 'Str.Plosnitei, bl.34, sc.A',
             'phone' => '0749987991'
+        ]);
+
+        $settingPatient2 = $patient2->settingsPatient()->create([
+            'pin' => '2740303458903',
+            'birthday' => Carbon::create(1974, 3, 03),
+            'gender' => 'f',
+            'country' => 'Romania',
+            'county' => 'Timis',
+            'city' => 'Timisoara',
+            'address' => 'Str.Plosnitei, bl.34, sc.A',
+            'phone' => '0749284591'
+        ]);
+
+        $settingPatient3 = $patient3->settingsPatient()->create([
+            'pin' => '2740303458745',
+            'birthday' => Carbon::create(1974, 3, 03),
+            'gender' => 'f',
+            'country' => 'Romania',
+            'county' => 'Timis',
+            'city' => 'Timisoara',
+            'address' => 'Str.Plosnitei, bl.34, sc.A',
+            'phone' => '0749284591'
         ]);
     }
 }
