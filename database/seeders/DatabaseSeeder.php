5db24730ec5c335e491f77206bc4d914b6704e80
Stefana Matei <stefana.otilia.matei@gmail.com>
2023-01-20 21:48:13 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            MedicPOVSeeder::class,
-            PatientPOVSeeder::class
+            MedicSeeder::class,
+            PatientSeeder::class
         ]);
 
     }
