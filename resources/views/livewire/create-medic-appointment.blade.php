ce9e448da78db883604fe54971ea97a5a03c091a
Stefana Matei <stefana.otilia.matei@gmail.com>
2023-02-03 01:40:08 +0200

    Update create-medic-appointment.blade.php

--- resources/views/livewire/create-medic-appointment.blade.php
+++ resources/views/livewire/create-medic-appointment.blade.php
@@ -4,7 +4,7 @@
             @include('authenticated.components.medic', ['width' => 6, 'user' => $medic, 'showProfile' => false])
 
             <div class="col-md-6">
-                <label>Selectati data dorită</label>
+                <label>Selectați data dorită</label>
                 <livewire:calendar-select/>
             </div>
         </div>
